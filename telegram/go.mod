891fce1be7cd2e865fe2665930ecc08c1415e769
Zeusina <kachusov_k@outlook.com>
2024-02-19 21:02:53 +0300

    feat: Added telegram bot configuration and launch

---
+++ telegram/go.mod
@@ -0,0 +1,3 @@
+module github.com/Zeusina/khlebnik-bot/telegram
+
+go 1.22.0
---
+++ telegram/start_bot.go
@@ -0,0 +1,30 @@
+package telegram
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/Zeusina/khlebnik-bot/telegram/handlers"
+	"github.com/go-telegram/bot"
+	log "github.com/sirupsen/logrus"
+)
+
+func StartBot() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	var opts []bot.Option
+	if os.Getenv("DEBUG") == "1" {
+		opts = append(opts, bot.WithDebug())
+		log.Debug("Configure bot to run in debug mode")
+	}
+
+	b, err := bot.New(os.Getenv("TOKEN"), opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypePrefix, handlers.StartHandler)
+
+	b.Start(ctx)
+}
