4126322da854f171034f6fe5e4c8be4b76b191bb
Zeusina <kachusov_k@outlook.com>
2024-03-04 20:48:40 +0300

    feat: added new handler for request question

---
+++ telegram/handlers/question_request_handler.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/Zeusina/khlebnik-bot/utils"
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	log "github.com/sirupsen/logrus"
+)
+
+func QuesionRequestHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	log.Debugf("User %d wants to ask question", update.Message.From.ID)
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      update.Message.Chat.ID,
+		Text:        utils.GetMessage("questionrequest"),
+		ReplyMarkup: models.ReplyKeyboardRemove{},
+	})
+}
