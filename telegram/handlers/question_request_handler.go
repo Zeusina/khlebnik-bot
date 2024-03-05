7125c44e1f4efce00743d3f48b5cb102a837aeeb
Zeusina <kachusov_k@outlook.com>
2024-03-05 20:18:23 +0300

    feat: :sparkles: added ability to work with fsm to question_request_handler

--- telegram/handlers/question_request_handler.go
+++ telegram/handlers/question_request_handler.go
@@ -11,10 +11,12 @@ import (
 
 func QuesionRequestHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	log.Debugf("User %d wants to ask question", update.Message.From.ID)
-
+	utils.SetFSM("askquestion", update.Message.From.ID)
 	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID:      update.Message.Chat.ID,
-		Text:        utils.GetMessage("questionrequest"),
-		ReplyMarkup: models.ReplyKeyboardRemove{},
+		ChatID: update.Message.Chat.ID,
+		Text:   utils.GetMessage("questionrequest"),
+		ReplyMarkup: models.ReplyKeyboardRemove{
+			RemoveKeyboard: true,
+		},
 	})
 }
