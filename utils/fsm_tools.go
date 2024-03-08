54420934b1ce49bdb868af8ca8c91aaa588853c2
Zeusina <kachusov_k@outlook.com>
2024-03-08 17:43:59 +0300

    fix: :technologist: added more detailed log with fields

--- utils/fsm_tools.go
+++ utils/fsm_tools.go
@@ -1,5 +1,7 @@
 package utils
 
+import log "github.com/sirupsen/logrus"
+
 var fsm map[int64]string
 
 func InitFSM() {
@@ -7,9 +9,14 @@ func InitFSM() {
 }
 
 func GetFSM(userId int64) string {
+	log.WithField("userid", userId).Debug("Get FSM state for user")
 	return fsm[userId]
 }
 
 func SetFSM(value string, userId int64) {
+	log.WithFields(log.Fields{
+		"userid":   userId,
+		"newstate": value,
+	}).Debug("Set new FSM state for user")
 	fsm[userId] = value
 }
