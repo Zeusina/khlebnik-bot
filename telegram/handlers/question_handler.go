2c3aa4c391b680089eea9084acf22b1db93aad1b
Zeusina <kachusov_k@outlook.com>
2024-03-09 18:59:39 +0300

    feat: :sparkles: added answer for question using ai api

--- telegram/handlers/question_handler.go
+++ telegram/handlers/question_handler.go
@@ -2,6 +2,12 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
 
 	"github.com/Zeusina/khlebnik-bot/utils"
 	"github.com/go-telegram/bot"
@@ -16,10 +22,82 @@ func QuestionFSMCheck(update *models.Update) bool {
 
 func QuestionHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	log.Debugf("%d used /start command", update.Message.From.ID)
-	b.SendMessage(ctx, &bot.SendMessageParams{
+	questionMessage, _ := b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
 		Text:   utils.GetMessage("questionaccepted"),
 	})
 	utils.SetFSM("none", update.Message.From.ID)
-	// TODO: add interaction with model here
+
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "localhost"
+		log.Warn("API host not set, using 'localhost'")
+	}
+	baseurl, err := url.Parse(fmt.Sprintf("http://%s:8080", host))
+	if err != nil {
+		log.Error(err)
+	}
+	baseurl.Path += "predict"
+	params := url.Values{}
+	params.Add("question", update.Message.Text)
+	baseurl.RawQuery = params.Encode()
+
+	question_predict, err := http.Get(baseurl.String())
+
+	if err != nil {
+		log.Error(err)
+	}
+	defer question_predict.Body.Close()
+	body, err := io.ReadAll(question_predict.Body)
+	if err != nil {
+		log.Error(err)
+	}
+
+	var bodyStruct []answerPredict
+
+	json.Unmarshal(body, &bodyStruct)
+	log.WithField("content", bodyStruct).Debug("get prediction for question")
+
+	baseurl, err = url.Parse(fmt.Sprintf("http://%s:8080/answer/%d", host, bodyStruct[0].CorpusId))
+	if err != nil {
+		log.Error(err)
+	}
+
+	var answer questionAnswer
+
+	questionAnswerResponse, err := http.Get(baseurl.String())
+	if err != nil {
+		log.Error(err)
+	}
+	body, err = io.ReadAll(questionAnswerResponse.Body)
+	if err != nil {
+		log.Error(err)
+	}
+	defer questionAnswerResponse.Body.Close()
+	err = json.Unmarshal(body, &answer)
+	if err != nil {
+		log.Error(err)
+	}
+	log.WithField("content", string(body)).Debug("Get answer for question")
+	_, err = b.EditMessageText(ctx, &bot.EditMessageTextParams{
+		MessageID: questionMessage.ID,
+		ChatID:    questionMessage.Chat.ID,
+		Text:      answer.Answer,
+	})
+	if err != nil {
+		log.Error(err)
+	}
+	log.WithField("messagetext", answer.Answer).Debug("Message edited")
+}
+
+type answerPredict struct {
+	CorpusId       int     `json:"corpus_id"`
+	Score          float64 `json:"score"`
+	CorpusQuestion string  `json:"corpus_quesion"`
+}
+
+type questionAnswer struct {
+	Id       int    `json:"id"`
+	Question string `json:"int"`
+	Answer   string `json:"answer"`
 }
